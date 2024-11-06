e9ee2c52a4f1fda958ef9c215b7c86e14f69fb1e
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2024-11-06 16:48:21 -0500

    Create roles.tf

---
+++ roles.tf
@@ -0,0 +1,17 @@
+resource "azurerm_role_definition" "reader_role" {
+  name        = "Reader"
+  scope       = "/subscriptions/${data.azurerm_subscription.primary.id}"
+  description = "Can read everything"
+  permissions {
+    actions = [
+      "Microsoft.Resources/subscriptions/resourceGroups/read",
+      "Microsoft.Resources/subscriptions/read"
+    ]
+  }
+  assignable_scopes = ["/subscriptions/${data.azurerm_subscription.primary.id}"]
+}
+resource "azurerm_role_assignment" "reader_assignment" {
+  principal_id           = "user-or-service-principal-id"
+  role_definition_name   = azurerm_role_definition.reader_role.name
+  scope                  = "/subscriptions/${data.azurerm_subscription.primary.id}"
+}
