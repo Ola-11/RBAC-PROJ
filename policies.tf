55ba37152797cec10be70ae71588f6da38f2ac18
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2024-11-06 16:48:53 -0500

    Create policies.tf

---
+++ policies.tf
@@ -0,0 +1,25 @@
+resource "azurerm_policy_definition" "enforce_tags" {
+  name         = "enforce-tags"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Enforce resource tagging"
+  description  = "Ensure all resources have the required tags"
+  metadata     = jsonencode({
+    category = "Tags"
+  })
+  policy_rule  = jsonencode({
+    "if" = {
+      "field" = "tags"
+      "equals" = {}
+    }
+    "then" = {
+      "effect" = "deny"
+    }
+  })
+}
+
+resource "azurerm_policy_assignment" "assign_tags_policy" {
+  name                 = "assign-tags-policy"
+  policy_definition_id = azurerm_policy_definition.enforce_tags.id
+  scope                = "/subscriptions/${data.azurerm_subscription.primary.id}"
+}
