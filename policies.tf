f64046b61247fb5ad83f0a913f9fd53b2963b425
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2024-11-06 17:01:25 -0500

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  features {}
+  version = "~> 5.0"  # Use an updated version for compatibility
+}
+
 resource "azurerm_policy_definition" "enforce_tags" {
   name         = "enforce-tags"
   policy_type  = "Custom"
